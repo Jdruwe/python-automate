d0ca550628bf6b639412cc378c54d587b07a0f32
Jeroen <druwe.jeroen@gmail.com>
2017-07-06 21:59:50 +0200

    flows and functions

---
+++ flow.py
@@ -0,0 +1,37 @@
+# name = 'derrr'
+# if name == 'Alice':
+#     print('Hi Alice')
+# elif name == 'Derp':
+#     print('Hi Derp')
+# else:
+#     print('Hi :D')
+# print('done')
+
+# spam = 1
+# while spam <= 5:
+#     print(spam)
+#     if spam == 4:
+#         break
+#     spam = spam + 1
+#     if (spam - 1) % 2 == 0:
+#         print('even')
+#         continue
+#     print('uneven')
+
+# for i in range(5):
+#     print(i)
+
+# total = 0
+#
+# for i in range(101):
+#     total = total + i
+# print(total)
+
+# for i in range(10, 20):
+#     print(i)
+
+# for i in range(10, 20, 2):
+#     print(i)
+
+for i in range(5, -1, -1):
+    print(i)
---
+++ functions.py
@@ -0,0 +1,55 @@
+# import random
+# print(random.randint(1, 10))
+
+# from random import *
+# print(randint(1, 10))
+
+# import sys
+# print('hello')
+# sys.exit()
+# print('goodbye')
+
+# import os
+# import sys
+# os.path.dirname(sys.executable)
+# 'C:\\Python25'
+
+# import pyperclip
+# pyperclip.copy('Hello world')
+# print(pyperclip.paste())
+
+
+# def printy(text):
+#     print(text)
+# printy('Hey')
+
+
+# def increase(number, by):
+#     return number + by
+#
+# print(increase(10, 5))
+
+# print('Hello')
+# print('world')
+# print('Hello', end='')
+# print(' world')
+
+# print('Hello', 'world', sep='ABC')
+
+# def spam():
+#     eggs = 'Hello'
+#     print(eggs)
+#
+# eggs = 42
+# spam()
+# print(eggs)
+
+# def spam():
+#     global eggs
+#     eggs = 'Hello'
+#     print(eggs)
+#
+# eggs = 42
+# spam()
+# print(eggs)
+
